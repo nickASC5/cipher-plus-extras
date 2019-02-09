f9ae2b8ff5067474b836b8439621999405f28fa8
nickASC5 <41080205+nickASC5@users.noreply.github.com>
2019-02-08 20:43:09 -0500

    added html + js files

---
+++ main.js
@@ -0,0 +1,55 @@
+var inputs = document.getElementsByTagName("input")
+var buttons = document.getElementsByTagName("button")
+var result = document.getElementById("result")
+
+function encode(event,isDecoding) {
+	
+	var emptyInputs = false;
+	for (var i = 0; i < inputs.length; i++)
+		if (!inputs[i].value){
+			emptyInputs = true;
+			console.log("input " + i + " is empty")
+		}
+	if (!emptyInputs) {
+		var text = inputs[0].value.toLowerCase();
+		var key = parseInt(inputs[1].value);
+		if (isDecoding) key = key;
+		var output = doTheWork(text, key, isDecoding);
+		console.log(output);
+		result.innerHTML = output;
+	}
+}
+
+buttons[0].addEventListener('click', function() {encode(event, false)});
+buttons[1].addEventListener('click', function() {encode(event, true)});
+
+
+function genCipher(num, isDecoding) {
+	var letters = 'abcdefghijklmnopqrstuvwxyz1234567890';
+	//codePairs = []
+	var codeDict = {};
+	for (var i = 0; i < letters.length; i++)
+		if (!isDecoding)
+			codeDict[letters.charAt(i)] = letters.charAt((i+num) % letters.length);
+		else
+			codeDict[letters.charAt((i+num) % letters.length)] = letters.charAt(i);
+	//	codePairs.push([letters.charAt(i),letters.charat((i+num) % letters.length)])
+	return codeDict
+}
+
+function scramble(string,codeDict) {
+	var newStr = '';
+	for(var i = 0; i < string.length; i++) {
+		if (Object.keys(codeDict).indexOf(string.charAt(i)) > -1) 
+			newStr += codeDict[string.charAt(i)];
+		else 
+			newStr += string.charAt(i);
+	}
+	return newStr;
+}
+
+function doTheWork(string, key, isDecoding) {
+	return scramble(string, genCipher(key, isDecoding));
+}
+
+
